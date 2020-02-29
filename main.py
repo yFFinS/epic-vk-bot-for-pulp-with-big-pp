75ca402145d3fbe1dfd13a6eaa41ee644fb0d941
Name <43855058+yFFinS@users.noreply.github.com>
2020-02-29 22:06:13 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -7,14 +7,17 @@ import urllib.request
 
 import colorama
 import vk_api
+from PIL import Image
 from vk_api.bot_longpoll import *
 
 access_token = 'token'
-group_id = 'id'
+group_id = 'bot_id'
+peer_id = '2000000001'
 longpoll = None
 vk = None
 vk_session = None
 send_mode = False
+upload = None
 traceback_mode = 1
 images_cache = set()
 messages_cache = set()
@@ -47,9 +50,9 @@ def main():
 
 @status
 def init(access_token, group_id):
-    global vk_session, longpoll, vk
+    global vk_session, longpoll, upload
     vk_session = vk_api.VkApi(token=access_token)
-    vk = vk_session.get_api()
+    upload = vk_api.VkUpload(vk_session)
     longpoll = VkBotLongPoll(vk_session, group_id=group_id)
     return longpoll
 
@@ -96,18 +99,55 @@ def listen_events(longpoll):
 
 @status
 def create_demotivator():
-    pass
+    text = [random.choice(list(messages_cache)).strip().split() for i in range(4)]
+    ind = [0, 0, 0, 0]
+    for i in range(2):
+        length = len(text[i])
+        if length != 1:
+            ind[i] = random.randint(1, length)
+        else:
+            ind[i] = 1
+    for i in range(2, 4):
+        length = len(text[i])
+        if length != 1:
+            ind[i] = random.randint(0, len(text[i]) - 1)
+        else:
+            ind[i] = 0
+    header = ' '.join(i for i in text[0][:ind[0]]) + ' ' + ' '.join(i for i in text[1][ind[2]:])
+    urmom = ' '.join(i for i in text[2][:ind[1]]) + ' ' + ' '.join(i for i in text[3][ind[3]:])
+    '''text_iamge = Image.new('RGB', (200, 100), (0, 0, 0))
+    drawer = ImageDraw.Draw(text_iamge)
+    drawer.text((20, 20), header, fill=(255, 255, 255))
+    drawer.text((20, 50), urmom, fill=(255, 255, 255))
+
+    text_offset = (50, 50)
+    meme_offset = (50, 10)
+    sample.paste(text_iamge, text_offset)
+
+    meme = Image.open(f'database/{meme}.jpg')
+    sample.paste(meme, meme_offset)'''
+    meme = 'database/images/' + random.choice(list(images_cache))
+    url = vk_session.method('photos.getMessagesUploadServer', {'peer_id': peer_id})['upload_url']
+    pfile = requests.post(url, files={'photo': open(meme, 'rb')}).json()
+    photo = vk_session.method('photos.saveMessagesPhoto',
+                              {'server': pfile['server'],
+                               'photo': pfile['photo'],
+                               'hash': pfile['hash']}
+                              )[0]
+    send_demotivator(str(photo['owner_id']), str(photo['id']))
+    send(header + '\n' + urmom)
+    return True
 
 
 @status
 def load_images_to_database(images):
     for data in images:
         if data['type'] == 'photo':
-            name = data['photo']['access_key']
+            name = data['photo']['access_key'] + '.jpg'
             if name in images_cache:
                 return
-            url = data['photo']['sizes'][0]['url']
-            urllib.request.urlretrieve(url, f"database/images/{name}.jpg")
+            url = data['photo']['sizes'][-1]['url']
+            urllib.request.urlretrieve(url, f"database/images/{name}")
             images_cache.add(name)
 
 
@@ -117,7 +157,7 @@ def load_text_to_database(text):
     if text in messages_cache:
         return
     with open('database/messages.txt', 'a') as file:
-        file.write(text)
+        file.write('\n' + text)
     messages_cache.add(text)
 
 
@@ -139,7 +179,8 @@ def convert_to_time(date):
 def get_cache():
     global messages_cache, images_cache
     with open('database/messages.txt', 'r') as file:
-        messages_cache = set(file.readlines())
+        messages_cache = set(i for i in map(str.strip, file.readlines()) if i)
+        print(messages_cache)
 
     images_cache = set(os.listdir('database/images'))
 
@@ -204,8 +245,15 @@ def traceback(mode):
 @status
 def send(text):
     vk_session.method('messages.send', {'random_id': random.getrandbits(32),
-                                        'peer_id': 2000000001,
+                                        'peer_id': peer_id,
                                         'message': text})
+
+
+def send_demotivator(owner, id):
+    vk_session.method('messages.send', {'random_id': random.getrandbits(32),
+                                        'peer_id': peer_id,
+                                        'message': 'bruh',
+                                        'attachment': f'photo{owner}_{id}'})
 
 
 def switch_send_mode():
@@ -217,7 +265,7 @@ def switch_send_mode():
     send_mode = not send_mode
 
 
-COMMANDS = {'bruh': lambda: True}
+COMMANDS = {'gen': create_demotivator}
 TERMINAL_COMMANDS = {'send': switch_send_mode, 'exit': terminate, 'traceback': traceback}
 if __name__ == '__main__':
     colorama.init(autoreset=True)
